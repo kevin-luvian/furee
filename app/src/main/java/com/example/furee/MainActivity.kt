5c7d9e69f06e041077b6937b099a20c5a86184d9
kevin-luvian <kevinlh90@gmail.com>
2021-10-03 19:38:50 +0700

    refactor: change content with Navigation composable

--- app/src/main/java/com/example/furee/MainActivity.kt
+++ app/src/main/java/com/example/furee/MainActivity.kt
@@ -3,17 +3,7 @@ package com.example.furee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.furee.appointment.view.AppointmentView
-import com.example.furee.ui.component.BillView
-import com.example.furee.ui.component.ButtonIcon
-import com.example.furee.ui.component.ImageRadioOption
+import com.example.furee.navigation.Navigation
 import com.example.furee.ui.theme.FureeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FureeTheme {
-                AppointmentView()
+                Navigation()
             }
         }
     }
