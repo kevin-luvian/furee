3e23a5c3703a47d8963ae49c4d7d313b9bcda150
kevin luvian <kevinluvian@pop-os.localdomain>
2021-10-03 14:21:08 +0700

    refactor: change main content with appointment view

--- app/src/main/java/com/example/furee/MainActivity.kt
+++ app/src/main/java/com/example/furee/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.furee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.furee.appointment.view.AppointmentView
+import com.example.furee.ui.component.BillView
+import com.example.furee.ui.component.ButtonIcon
+import com.example.furee.ui.component.ImageRadioOption
 import com.example.furee.ui.theme.FureeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +21,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FureeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                AppointmentView()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FureeTheme {
-        Greeting("Android")
     }
 }
