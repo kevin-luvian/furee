7ce81f63627804394d6306e61a3827cef278ac11
kevin luvian <kevinluvian@pop-os.localdomain>
2021-10-03 14:00:01 +0700

    refactor: change theme color

--- app/src/main/java/com/example/furee/ui/theme/Color.kt
+++ app/src/main/java/com/example/furee/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.example.furee.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkBlue = Color(0xFF202c3f)
+val LightGray = Color(0xFFeaeaea)
+val DarkGray = Color(0xFF565e68)
+val LightOrange = Color(0xFFee835b)
+val Cream = Color(0xFFfef8f6)
