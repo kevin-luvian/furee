f90e2e58e1a501d8ea8aad6765946a2d2aa0420e
kevin-luvian <kevinlh90@gmail.com>
2021-10-04 11:51:40 +0700

    refactor: use common screen surface

--- app/src/main/java/com/example/furee/checkout/CheckoutScreen.kt
+++ app/src/main/java/com/example/furee/checkout/CheckoutScreen.kt
@@ -1,13 +1,9 @@
 package com.example.furee.checkout
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,42 +13,30 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.furee.navigation.NavScreen
+import com.example.furee.ui.component.ScreenSurface
 
 object CheckoutScreen : NavScreen("checkout") {
     @Composable
     fun View(navController: NavController, doctorName: String) {
-        Box(
-            Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background)
-                .verticalScroll(rememberScrollState())
-        ) {
-            Surface(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(30.dp),
-                shape = RoundedCornerShape(15.dp),
-                elevation = 5.dp
+        ScreenSurface {
+            Column(
+                Modifier.padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Column(
-                    Modifier.padding(20.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = "Checkout",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Spacer(modifier = Modifier.padding(vertical = 10.dp))
-                    Text(
-                        text = "checkout an appointment with $doctorName",
-                        fontSize = 15.sp,
-                        fontWeight = FontWeight.Bold,
-                    )
-                    Spacer(modifier = Modifier.padding(vertical = 30.dp))
-                    Button({ navController.popBackStack() }) {
-                        Text("Go Back")
-                    }
+                Text(
+                    text = "Checkout",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.padding(vertical = 10.dp))
+                Text(
+                    text = "checkout an appointment with $doctorName",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Spacer(modifier = Modifier.padding(vertical = 30.dp))
+                Button({ navController.popBackStack() }) {
+                    Text("Go Back")
                 }
             }
         }
